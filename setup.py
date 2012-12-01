9a4077005d92ec5826024229511b4decba705bf7
roryk <rory.kirchner@gmail.com>
2012-11-30 23:25:16 -0500

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="chowda",
+      version="0.0.1",
+      author="Christine Miller",
+      author_email="chrismamil@gmail.com",
+      description="CLAMS data analyzer.",
+      url="https://github.com/chrismamil/chowda",
+      license="MIT",
+      namespace_packages=["chowda"],
+      packages=find_packages())
