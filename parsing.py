d973f225925cdea9808caf1a4cc1a6bce9aea93d
Christine Miller <chrismamil@gmail.com>
2013-01-14 23:15:22 -0500

    Added date parsing

--- parsing.py
+++ parsing.py
@@ -1,9 +1,23 @@
 #Split strip funciton
 #use this to get rid of the n\r and "" junk on the title text of each experiment
 
-
+import datetime
 
 def split_strip(title_text):
 	return title_text.strip('"').strip('n\r').split(" :")
 
 print split_strip('"Experiment Started: 13:46:31 11-07-11 n\r"')
+
+#Parse date
+#FYI this will give the wrong date after 2068 b/c values 0-68 are mapped to 2000-2068 in POISX or X/open standard 
+#http://docs.python.org/2/library/time.html
+
+def date_parse(x):
+	return(datetime.datetime.strptime(x, '%H:%M:%S %d-%m-%y'))
+
+print date_parse('13:46:31 11-07-11')
+
+
+
+
+
